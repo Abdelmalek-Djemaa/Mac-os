d6675fc00e76c3a6ccef09a24f4131f2a2d7c5ec
Abdelmalek <abdelmalek@MacBook-Pro-de-Abdelmalek.local>
2024-12-04 11:02:01 +0100

    update

--- src/components/Apps/Camera.jsx
+++ src/components/Apps/Camera.jsx
@@ -23,25 +23,31 @@ const Camera = ({ setShowCamera }) => {
         const stream = videoRef.current.srcObject;
         const tracks = stream.getTracks();
         tracks.forEach((track) => track.stop());
+        videoRef.current.srcObject = null;
       }
     };
   }, []);
 
   return (
     <div className="relative bg-black max-w-xl w-full min-h-[400px] flex flex-col items-center justify-center rounded-xl z-[998]">
-        <div className="absolute top-0 left-0 mb-2 p-2.5 flex w-full bg-gray-800 rounded-t-xl z-[999]">
-            <div
-              className="group sm:h-3 sm:w-3 h-2.5 w-2.5 bg-red-500 rounded-full flex justify-center items-center cursor-pointer"
-              onClick={() => setShowCamera(false)}
-            >
-              <span className="hidden group-hover:block sm:text-[10px] text-[8px] text-gray-900 font-bold">
-                <FaXmark />
-              </span>
-            </div>
-            <div className="ml-2 sm:h-3 sm:w-3 h-2.5 w-2.5 bg-orange-300 rounded-full"></div>
-            <div className="ml-2 sm:h-3 sm:w-3 h-2.5 w-2.5 bg-green-500 rounded-full"></div>
+      <div className="absolute top-0 left-0 mb-2 p-2.5 flex w-full bg-gray-800 rounded-t-xl z-[999]">
+        <div
+          className="group sm:h-3 sm:w-3 h-2.5 w-2.5 bg-red-500 rounded-full flex justify-center items-center cursor-pointer"
+          onClick={() => setShowCamera(false)}
+        >
+          <span className="hidden group-hover:block sm:text-[10px] text-[8px] text-gray-900 font-bold">
+            <FaXmark />
+          </span>
         </div>
-      <video ref={videoRef} autoPlay playsInline style={{ width: '100%', height: 'auto', borderRadius: '0.75rem' }} />
+        <div className="ml-2 sm:h-3 sm:w-3 h-2.5 w-2.5 bg-orange-300 rounded-full"></div>
+        <div className="ml-2 sm:h-3 sm:w-3 h-2.5 w-2.5 bg-green-500 rounded-full"></div>
+      </div>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        style={{ width: '100%', height: 'auto', borderRadius: '0.75rem' }}
+      />
     </div>
   );
 };
