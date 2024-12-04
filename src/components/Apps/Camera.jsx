0941df2c3b137241a607fd5574a28df0a3d8a08e
Abdelmalek <abdelmalek@MacBook-Pro-de-Abdelmalek.local>
2024-12-04 13:29:58 +0100

    update

--- src/components/Apps/Camera.jsx
+++ src/components/Apps/Camera.jsx
@@ -19,26 +19,35 @@ const Camera = ({ setShowCamera }) => {
     startCamera();
 
     return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        const stream = videoRef.current.srcObject;
-        const tracks = stream.getTracks();
-        tracks.forEach((track) => track.stop());
-        videoRef.current.srcObject = null;
-      }
+      stopCamera();
     };
   }, []);
 
+  const stopCamera = () => {
+    if (videoRef.current && videoRef.current.srcObject) {
+      const stream = videoRef.current.srcObject;
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+      videoRef.current.srcObject = null;
+    }
+  };
+
+  const handleExit = () => {
+    stopCamera();
+    setShowCamera(false);
+  };
+
   return (
     <div className="relative bg-black max-w-xl w-full h-full sm:max-h-[430px] max-h-[330px] flex flex-col items-center justify-center rounded-xl z-[998]">
-      <div className='absolute top-2 w-full flex justify-center items-center'>
-        <span className='font-medium text-sm text-white z-[1000]'>
-        Photo Booth
+      <div className="absolute top-2 w-full flex justify-center items-center">
+        <span className="font-medium text-sm text-white z-[1000]">
+          Photo Booth
         </span>
       </div>
       <div className="absolute top-0 left-0 p-2.5 flex w-full bg-gray-800 rounded-t-xl z-[999]">
         <div
           className="group sm:h-3 sm:w-3 h-2.5 w-2.5 bg-red-500 rounded-full flex justify-center items-center cursor-pointer"
-          onClick={() => setShowCamera(false)}
+          onClick={handleExit}
         >
           <span className="hidden group-hover:block sm:text-[10px] text-[8px] text-gray-900 font-bold">
             <FaXmark />
@@ -54,8 +63,8 @@ const Camera = ({ setShowCamera }) => {
         style={{ width: '100%', height: '100%', borderRadius: '0.75rem' }}
       />
       <div className="absolute bottom-0 left-0 sm:py-3 py-2 flex justify-center items-center w-full bg-gray-600 bg-opacity-45 rounded-b-xl z-[999]">
-        <div className='flex justify-center items-center sm:w-[40px] sm:h-[40px] w-[35px] h-[35px] rounded-full bg-red-500'>
-          <FaCamera className='text-white' />
+        <div className="flex justify-center items-center sm:w-[40px] sm:h-[40px] w-[35px] h-[35px] rounded-full bg-red-500">
+          <FaCamera className="text-white" />
         </div>
       </div>
     </div>
