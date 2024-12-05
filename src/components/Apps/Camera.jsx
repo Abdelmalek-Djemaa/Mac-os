bd4e68d988a07a48a593953e63290c74c39e4941
Abdelmalek <abdelmalek@MacBook-Pro-de-Abdelmalek.local>
2024-12-05 08:04:28 +0100

    update

--- src/components/Apps/Camera.jsx
+++ src/components/Apps/Camera.jsx
@@ -41,16 +41,16 @@ const Camera = ({ setShowCamera }) => {
 
   return (
     <div className='fixed top-0 bottom-0 right-0 left-0 flex justify-center items-center p-4'>
-      <Draggable nodeRef={cameraRef}>
+      <Draggable nodeRef={cameraRef} cancel=".iteractive">
         <div className="relative bg-black max-w-xl w-full h-full sm:max-h-[430px] max-h-[330px] flex flex-col items-center justify-center rounded-xl z-[998]" ref={cameraRef}>
           <div className="absolute top-2 w-full flex justify-center items-center">
             <span className="font-medium text-sm text-white z-[1000]">
               Photo Booth
             </span>
           </div>
-          <div className="absolute top-0 left-0 p-2.5 flex w-full bg-gray-800 rounded-t-xl z-[999]">
+          <div className="iteractive absolute top-0 left-0 p-2.5 flex w-full bg-gray-800 rounded-t-xl z-[999]">
             <div
-              className="group sm:h-3 sm:w-3 h-2.5 w-2.5 bg-red-500 rounded-full flex justify-center items-center cursor-pointer"
+              className="group sm:h-3 sm:w-3 h-2.5 w-2.5 bg-red-500 rounded-full flex justify-center items-center cursor-pointer z-[1000]"
               onClick={handleExit}
             >
               <span className="hidden group-hover:block sm:text-[10px] text-[8px] text-gray-900 font-bold">
@@ -67,7 +67,7 @@ const Camera = ({ setShowCamera }) => {
             style={{ width: '100%', height: '100%', borderRadius: '0.75rem' }}
           />
           <div className="absolute bottom-0 left-0 sm:py-3 py-2 flex justify-center items-center w-full bg-gray-800 rounded-b-xl z-[999]">
-            <div className="flex justify-center items-center sm:w-[40px] sm:h-[40px] w-[35px] h-[35px] rounded-full bg-red-500">
+            <div className="iteractive flex justify-center items-center sm:w-[40px] sm:h-[40px] w-[35px] h-[35px] rounded-full bg-red-500">
               <FaCamera className="text-white" />
             </div>
           </div>
--- src/components/main/Folder.jsx
+++ src/components/main/Folder.jsx
@@ -13,13 +13,13 @@ const Folder = ({ setShowFolder, folderName }) => {
 
   return (
     <div className="fixed w-full h-full flex justify-center items-center p-4">
-      <Draggable nodeRef={finderRef}>
+      <Draggable nodeRef={finderRef} cancel=".iteractive">
         <div className="flex flex-row justify-center items-center h-full w-full" ref={finderRef}>
           <div className="flex flex-col justify-start items-start sm:max-w-[200px] max-w-[100px] shadow-sm shadow-black w-full min-h-[50%] rounded-l-xl bg-gray-700">
-            <div className="top-0 left-0 mb-2 p-4 flex relative">
+            <div className="iteractive top-0 left-0 mb-2 p-4 flex relative">
               <div
                 className="group sm:h-3 sm:w-3 h-2.5 w-2.5 bg-red-500 rounded-full flex justify-center items-center cursor-pointer z-10"
-                onClick={setShowFolder}  // Close button functionality
+                onClick={setShowFolder} 
               >
                 <span className="hidden group-hover:block sm:text-[10px] text-[8px] text-gray-900 font-bold">
                   <FaXmark />
@@ -28,7 +28,7 @@ const Folder = ({ setShowFolder, folderName }) => {
               <div className="ml-2 sm:h-3 sm:w-3 h-2.5 w-2.5 bg-orange-300 rounded-full"></div>
               <div className="ml-2 sm:h-3 sm:w-3 h-2.5 w-2.5 bg-green-500 rounded-full"></div>
             </div>
-            <ul className="w-full">
+            <ul className="iteractive w-full">
               <li className="w-full py-2 px-3 text-white">
                 <div className="sm:text-[10px] text-[7px] text-gray-500 font-medium">Skills</div>
               </li>
@@ -46,7 +46,7 @@ const Folder = ({ setShowFolder, folderName }) => {
           <div className="folder-content flex flex-col justify-center items-center max-w-lg relative w-full text-white min-h-[50%] rounded-r-xl bg-gray-800 shadow-sm shadow-black">
             <nav className="flex flex-row top-0 justify-between items-center w-full px-3 py-4 absolute border-b border-opacity-30 border-black">
               <span className="font-bold text-sm">{folderName}</span>
-              <ul className="flex items-center space-x-3 text-lg">
+              <ul className="iteractive flex items-center space-x-3 text-lg">
                 <li className="cursor-pointer font-bold">
                   <RiInformation2Line />
                 </li>
