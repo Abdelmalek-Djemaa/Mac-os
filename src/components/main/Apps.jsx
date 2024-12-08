2309fea95a19798da0d8737bb3324563d1c47442
Abdelmalek <abdelmalek@MacBook-Pro-de-Abdelmalek.local>
2024-12-08 16:53:58 +0100

    update

--- src/components/main/Apps.jsx
+++ src/components/main/Apps.jsx
@@ -7,7 +7,7 @@ import { FaSearch } from 'react-icons/fa';
 const Apps = ({setShowCamera}) => {
   return (
     <motion.div 
-      className="fixed w-full top-0 left-0 right-0 bottom-0 h-full flex justify-center items-center"
+      className="fixed w-full top-0 left-0 right-0 bottom-0 h-full flex justify-center items-center z-20"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
